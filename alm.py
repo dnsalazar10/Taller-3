2e540fc87846d691e7f12f6daf9b37c526132f69
dnsalazar10 <dn.salazar10@uniandes.edu.co>
2018-10-31 22:52:16 -0500

    Almacena imagen en array

---
+++ alm.py
@@ -0,0 +1,9 @@
+%matplotlib inline
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.fftpack
+from scipy import misc
+from scipy.fftpack import fft, ifft
+
+#Almacena la imagen Arbol.png
+im= misc.imread('Arbol.png')
