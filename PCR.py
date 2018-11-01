1c1aabacdac3c275ae30a86f877a776fd41e13b0
dnsalazar10 <dn.salazar10@uniandes.edu.co>
2018-10-31 21:05:54 -0500

    Se crea el documento PCR.py

--- PCR.py
+++ PCR.py
@@ -1,17 +1,21 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import numpy.linalg 
 import sklearn
+import numpy as np
 import mglearn
+from sklearn.datasets import load_breast_cancer
+import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
-from scipy.fftpack import fft, ifft, fftfreq
-from mpl_toolkits.mplot3d import Axes3D
+%matplotlib inline 
 
-#### Punto 2: PCA y PCR ####
-#Carga de datos
-datos = np.loadtxt('WDBC.dat',delimiter=',')
+cancer=load_breast_cancer()
+#Carga archivo WDBC.dat
+datos=np.loadtxt('WDBC1.dat', delimiter=',')
 
-#Funcion de matriz de covarianza 
+#Cambio de dimensión 31 a 2
+pca=PCA(n_components=2)
+pca.fit(datos)
+t=pca.transform(datos)
+
+#Imprime matriz de covarianza
 def cov_matrix(data):
     n_dim = np.shape(data)[1]
     n_points = np.shape(data)[0]
@@ -23,30 +27,10 @@ def cov_matrix(data):
             cov[i,j] = np.sum((data[:,i]-mean_i) * (data[:,j]-mean_j)) / (n_points -1)
     return cov
 
-#Cambio de dimensión 31 a 2
-pca=PCA(n_components=2)
-pca.fit(datos)
-t=pca.transform(datos)
-
 #Covarianza
 cov = cov_matrix(t)
 print(cov)
 
-#Vectores y valores propios
-val, vec = numpy.linalg.eig(cov)
 
-a1=val[0]
-a2=val[1]
-b1=vec[:,0]
-b2=vec[:,1]
 
-#Imprime a qué autovalor corresponde cada autovector
-print(f'Autovector: {b1} y Autovalor: {a1}')
-print(f'Autovector: {b2} y Autovalor: {a2}')
 
-#Proyección en PC1 y PC2
-mglearn.discrete_scatter(t[:,0],t[:,1])
-plt.legend(loc='best')
-plt.xlabel('PCA 1')
-plt.ylabel('PCA 2')
-#plt.savefig(SalazarNicole_PCA.pdf)
